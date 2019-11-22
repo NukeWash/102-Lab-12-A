c04d5cf842c988f07d3fd191054b07676ce0c239
John <johnhennon@mymail.mines.edu>
2019-11-22 14:41:01 -0700

    Week12-utility.py

--- Week12-utility.py
+++ Week12-utility.py
@@ -30,13 +30,17 @@ def FindWordCount(List_In,String_Check):
     return Count
 
 #E ScoreFinder
-''' is a function that takes in two lists and a string. The first list is a list of strings (player names), the second list is a list of floats (player scores), and the string is a name (player to find). If the player to find exists in the list of player names, then print the name of that player along with their associated score (which is in the second list at the same index). If the player to find does not exist in the list of player names, print player not found. Here are two examples of using this function:
->>> players = ["Mary", "Cody", "Joe", "Jill", "Xai", "Bodo"]
->>> scores = [5, 8, 10, 6, 10, 4]
->>> ScoreFinder(players, scores, "jill") % NOTE: upper and lowercase should both work
-OUTPUT Jill got a score of 6
->>> ScoreFinder(players, scores, "Manuel")
-OUTPUT player not found'''
+def ScoreFinder(List_Names,List_Floats,Name):
+    x=0
+    for i in range(len(List_Names)):
+        if List_Names[i].lower()==Name.lower():
+            j=i
+            print("OUTPUT",Name," scored ",List_Floats[j]," points")
+            x=(-10000)
+        else:
+            x+=1
+    if x>0:
+        print("OUTPUT Player not found")
 
          
     
